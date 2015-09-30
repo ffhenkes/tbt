4fee035fcbff34d61b3ea2bbc3f417939d25b4a2
Joker <fabio.favero@gmail.com>
2015-09-29 21:53:34 -0300

    [ADD] complete skeleton

--- controller/simple_controller.php
+++ controller/simple_controller.php
@@ -2,11 +2,20 @@
 
 // simple controller to interact with dao
 
-$dao = new SimpleDao();
-$res = $dao->get_some();
+class SimpleController {
 
-// debug
-echo 'Some result: \n';
-var_dump($res);
+  private $dao;
+
+  public function __construct() {
+    $this->dao = new SimpleDao();
+  }
+
+  public function get_control_of_some() {
+    
+    return $this->dao->get_some()
+    
+  }
+
+}
 
 ?>
--- throwback.php
+++ throwback.php
@@ -4,4 +4,11 @@ require_once('startup.php');
 
 // do something for start
 
+$control = new SimpleController();
+$res = $control->get_control_of_some();
+
+// debug
+echo 'Some result: \n';
+var_dump($res);
+
 ?>
